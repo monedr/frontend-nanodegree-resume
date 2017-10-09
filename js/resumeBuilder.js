7de3a589c814b166275dfded0f4390d2c4e6be85
monedr <sdrutz@inf.ufpel.edu.br>
2017-10-09 16:10:04 -0300

    .

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,6 @@
 //$("#main").append(((299792458*100)/(1*(1/1000000000))));
-var firstName = "Simone";
-var age = 24;
-
+//var firstName = "Simone";
+//var age = 24;
+var awesomeThoughts;
+awesomeThoughts = "I am fucking awesome";
 console.log(firstName);
