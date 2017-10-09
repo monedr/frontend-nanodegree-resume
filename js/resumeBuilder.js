01eaf27dc67856d4521658a369b55a18132d5199
monedr <sdrutz@inf.ufpel.edu.br>
2017-10-09 17:56:21 -0300

    .

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,14 @@
 //$("#main").append(((299792458*100)/(1*(1/1000000000))));
 //var firstName = "Simone";
 //var age = 24;
+var firstName = "simone";
+var age  = 32;
+
+//console.log(firstName);
+
 var awesomeThoughts;
 awesomeThoughts = "I am fucking awesome";
-console.log(firstName);
+
+//console.log(awesomeThoughts);
+
+var email = "simone@udacity.com";
